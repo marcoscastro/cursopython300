746f60807ddb773c05a479dd64d4c9ca25db5442
Marcos Castro <mcastrosouza@live.com>
2017-01-25 04:12:15 -0300

    adicionada a pagina sobre

--- app/controllers/default.py
+++ app/controllers/default.py
@@ -7,3 +7,7 @@ from flask import render_template
 @app.route('/')
 def index():
     return render_template('index.html')
+
+@app.route('/sobre')
+def sobre():
+    return render_template('sobre.html')
